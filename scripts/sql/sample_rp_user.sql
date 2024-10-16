d5b8d6128eeba47d2f0e4519993da5585ba30345
Ravikanth <ravikanthreddy89@gmail.com>
2024-10-16 21:56:18 +0000

    [Db] Sample insert scripts from RP and user

--- scripts/sql/sample_rp_user.sql
+++ scripts/sql/sample_rp_user.sql
@@ -1,6 +1,4 @@
---- example.com
-
-INSERT INTO relying_parties (rp_id, name, origin, icon_url, description)
+INSERT INTO relying_parties (external_id, name, origin, icon_url, description)
 VALUES ('example.com', 'Example Service', 'https://example.com', 'https://example.com/icon.png', 'RP for example.com web traffic')
 RETURNING id;
 
@@ -26,10 +24,8 @@ INSERT INTO relying_party_configs (rp_id, setting_key, setting_value)
 VALUES (1, 'public_key_alg', '-7,-257,-37,-8,-35,-36');
 
 
-
--- netlify.app
-
-INSERT INTO relying_parties (rp_id, name, origin, icon_url, description)
+--- netlify app
+INSERT INTO relying_parties (external_id, name, origin, icon_url, description)
 VALUES ('pp-signal-sdk-demo.netlify.app', 'PP Signal netlify app', 'https://pp-signal-sdk-demo.netlify.app', 'https://example.com/icon.png', 'RP for example.com web traffic')
 RETURNING id;
 
@@ -53,3 +49,6 @@ VALUES (2, 'require_resident_key', 'true');
 INSERT INTO relying_party_configs (rp_id, setting_key, setting_value)
 VALUES (2, 'public_key_alg', '-7,-257,-37,-8,-35,-36');
 
+-- Sample user
+insert into users (id, external_id, username, email, display_name, created_at, updated_at)
+values (0, '65fUCTlqPlOSk22tkrkJ2m8I2MEhpF4fCI_pdosMAzk', 'testuser4', 'testuser4@netlify.app', 'TestUser4', NOW(), NOW());
